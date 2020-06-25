08e3b1dec6ba5751545630273c7b391039347f6a
cockochan <56898295+cockochan@users.noreply.github.com>
2020-06-25 16:21:43 +0300

    autoForward

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from 'react';
+import useInterval from 'react-useinterval';
+
 function App() {
   const [greatIndex, setGreatIndex] = useState(0);
   const [waiting, setWaiting]=useState()
+  const [action, setAction]=useState()
+  const [start, setStart]=useState(null)
+  useInterval(
+    () => {
+      handleForward()
+    },
+    start ? 500 : null
+  );
   const imageArray = [
     "./pics/394108_179405985495099_194997137_n.jpg",
     "./pics/395565_179405452161819_1380352913_n.jpg",
-    "./pics/395565_179405452161819_1380352913_n.jpg"
+    "./pics/419621_178440538924977_1347282475_n.jpg"
   ];
 
   const handleForward = () => {
     console.log(greatIndex)
     if (greatIndex < imageArray.length - 1) {
       let newIndex = greatIndex+1;
-      console.log(newIndex)
+    
       setGreatIndex(newIndex);
     } else {
-      // setGreatIndex(0);
-      console.log('zero')
+      setGreatIndex(0);
+      // console.log('zero')
     }
   };
 
@@ -30,22 +40,17 @@ function App() {
       
     }
   };
-
-  const handleAutoForward = () => {
-    if(waiting) {return}
-    const newInterval =  setInterval(() => {
-      handleForward();
-    }, 2000); 
-    setWaiting(newInterval)
-  };
+const handleAutoForward = () => {setStart(true)};
 
   const handleAutoBackward = () => {
     setTimeout(() => {
       handleBackward();
-      handleAutoBackward();
+   
     },2000);
   };
-
+const handleStop =()=>{
+  setStart(null)
+}
   // const handleStop= () => {
   //   setInterval(null)
   // };
@@ -59,6 +64,7 @@ function App() {
         <button onClick={handleBackward}>backward</button>
         <button onClick={handleForward}>forward</button>
         <button onClick={handleAutoForward}>Auto forward</button>
+        <button onClick={handleStop}>stop</button>
       </div>
     </div>
   );
