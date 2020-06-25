0aa26c45346119b025b209a46739126abf89c4eb
cockochan <56898295+cockochan@users.noreply.github.com>
2020-06-26 02:21:14 +0300

    ok.cool

--- src/App.js
+++ src/App.js
@@ -21,16 +21,17 @@ function App() {
   );
   const imageArray = [
     "./pics/jeka.jpg",
-    "./pics/pr.jpg",
+  
     "./pics/419621_178440538924977_1347282475_n.jpg",
-    "./pics/DSC_2093bc_.jpg",
+    "./pics/DSC_3665.jpg",
     "./pics/ALP_0392.png",
     './pics/50934388_2326403780925794_8182476207179169792_o.jpg',
     "./pics/katia.jpg",
     "./pics/kidaero.jpg",
-    "./pics/ALP_4008.png",
-    "./pics/ALP_4008.png",
-    './_CUB9815.jpg'
+    "./pics/19620133_10207321750321200_7708502522419890689_o.jpg",
+    "./pics/_CUB9815.jpg",
+    "./pics/zoya.jpg"
+
     
 
   ];
@@ -78,16 +79,16 @@ const handleStop =()=>{
       <div style={{width:'15px', height:'35px',backgroundColor:'orange', color:'white', fontWeight:'bolder'}}></div>
       <div style={{width:'15px', height:'35px',backgroundColor:'red', color:'white', fontWeight:'bolder'}}></div><div style={{width:'15px', height:'35px',backgroundColor:'violet', color:'white', fontWeight:'bolder'}}></div>
       <div style={{width:'15px', height:'35px',backgroundColor:'red', color:'white', fontWeight:'bolder'}}></div><div style={{width:'15px', height:'35px',backgroundColor:'violet', color:'white', fontWeight:'bolder'}}></div>
-      <p style={{fontWeight:'bolder', paddingTop:0, marginLeft:'15px', fontSize:'24px'}}>My photography gallery</p>
-      <div style={{height:'2px', width:'20%', alignSelf:'baseline', backgroundColor:'yellow', marginTop:'35px'}}></div>
-     </div></div>
+      <p style={{fontWeight:'bolder', color:'violet', paddingTop:0, marginLeft:'15px', fontSize:'24px'}}>My photography gallery</p>
+      
+     </div><div style={{height:'2px', width:'20%', alignSelf:'flex-end', backgroundColor:'blue', marginTop:'35px'}}></div></div>
       <img style={{marginLeft:'35px',height:'500px', width:'auto'}} alt='whateverYouWantToSee' className="image" src={imageArray[greatIndex]} />
       <div style={{marginLeft:'35px'}}>
-        <button onClick={handleAutoBackward}> auto backward</button>
-        <button onClick={handleBackward}>backward</button>
-        <button onClick={handleForward}>forward</button>
-        <button onClick={handleAutoForward}>Auto forward</button>
-        <button style={{backgroundColor:'red', color: 'white', fontWeight:'bolder', borderColor:'pink'}}onClick={handleStop}>stop(panik)</button>
+        <button style={{backgroundColor:'black',color:'white', bordr:'none',fontWeight:'bolder'}} onClick={handleAutoBackward}> auto backward</button>
+        <button style={{backgroundColor:'black',color:'white', bordr:'none',fontWeight:'bolder'}}onClick={handleBackward}>backward</button>
+        <button style={{backgroundColor:'black',color:'white', bordr:'none',fontWeight:'bolder'}}onClick={handleForward}>forward</button>
+        <button style={{backgroundColor:'black',color:'white', bordr:'none',fontWeight:'bolder'}}onClick={handleAutoForward}>Auto forward</button>
+        <button style={{backgroundColor:'red', color: 'white', fontWeight:'bolder', borderColor:'pink'}}onClick={handleStop}>stop</button>
         </div>
        
         </div>
